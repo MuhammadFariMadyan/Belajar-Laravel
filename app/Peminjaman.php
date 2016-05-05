de990f1fec39d12540779c3cc05b3f76de25e665
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-05 18:46:27 +0700

    tambah model peminjaman

---
+++ app/Peminjaman.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Peminjaman extends Model
+{
+    protected $table = 'tb_peminjaman';
+
+    public function buku(){
+        return $this->belongsTo('App\Buku');
+    }
+
+    public function mahasiswa(){
+        return $this->belongsTo('App\Mahasiswa');
+    }
+}
