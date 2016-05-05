bb32d89014dfcc6d1f7282d3cbcf948ec4311cf3
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-05 23:13:44 +0700

    tambah form simpan buku

---
+++ resources/views/buku/create.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.aplikasi')
+@section('title', 'Tambah Data Buku')
+@section('content')
+
+    {!! Form::open(array('url' => 'SimpanBuku')) !!}
+
+    <div class="form-group">
+        {!! Form::label('judul_buku', 'Judul Buku', ['class' => 'control-label']) !!}
+        {!! Form::text('judul_buku', null, ['class' => 'form-control', 'placeholder' => 'Masukkan Judul Buku']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('pengarang', 'Pengarang', ['class' => 'control-label']) !!}
+        {!! Form::text('pengarang', null, ['class' => 'form-control', 'placeholder' => 'Masukkan Nama Pengarang']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('tahun_terbit', 'Tahun Terbit', ['class' => 'control-label']) !!}
+        {!! Form::number('tahun_terbit', null, ['class' => 'form-control', 'placeholder' => 'Masukkan Tahun Terbit']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('penerbit', 'Penerbit', ['class' => 'control-label']) !!}
+        {!! Form::text('penerbit', null, ['class' => 'form-control', 'placeholder' => 'Masukkan Nama Penerbit']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('jumlah_buku', 'Jumlah Buku', ['class' => 'control-label']) !!}
+        {!! Form::number('jumlah_buku', null, ['class' => 'form-control', 'placeholder' => 'Masukkan Jumlah Buku']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('nomor_rak_buku', 'Nomor Rak Buku', ['class' => 'control-label']) !!}
+        {!! Form::text('nomor_rak_buku', null, ['class' => 'form-control', 'placeholder' => 'Masukkan Nomor Rak Buku']) !!}
+    </div>
+
+    {!! Form::submit('Simpan', ['class' => 'btn btn-primary']) !!}
+
+    {!! Form::close() !!}
+
+@endsection
