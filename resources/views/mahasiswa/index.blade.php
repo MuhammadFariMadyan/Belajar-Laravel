01ddd98f0d2170ab5584b1f0202f451f1f2f0e41
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-05 21:06:22 +0700

    format

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -2,29 +2,29 @@
 @section('title', 'Data Mahasiswa')
 @section('content')
 
-  <table class="table table-bordered table-hover table-responsive table-striped">
-    <thead>
-    <tr>
-      <th>NPM</th>
-      <th>Nama</th>
-      <th>Kelas</th>
-      <th>Jenis Kelamin</th>
-      <th>Alamat</th>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach($mahasiswas as $mahasiswa)
-      <tr>
-        <td>{{ $mahasiswa->npm }}</td>
-        <td>{{ $mahasiswa->nama }}</td>
-        <td>{{ $mahasiswa->kelas }}</td>
-        <td>{{ $mahasiswa->jenis_kelamin }}</td>
-        <td>{{ $mahasiswa->alamat }}</td>
-      </tr>
-    @endforeach
-    </tbody>
-  </table>
+    <table class="table table-bordered table-hover table-responsive table-striped">
+        <thead>
+        <tr>
+            <th>NPM</th>
+            <th>Nama</th>
+            <th>Kelas</th>
+            <th>Jenis Kelamin</th>
+            <th>Alamat</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($mahasiswas as $mahasiswa)
+            <tr>
+                <td>{{ $mahasiswa->npm }}</td>
+                <td>{{ $mahasiswa->nama }}</td>
+                <td>{{ $mahasiswa->kelas }}</td>
+                <td>{{ $mahasiswa->jenis_kelamin }}</td>
+                <td>{{ $mahasiswa->alamat }}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 
-  {!! $mahasiswas->render() !!}
+    {!! $mahasiswas->render() !!}
 
 @endsection
