8377ae71711f5b7455ca4db06d0458c93bed2fc5
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-05 18:46:12 +0700

    tambah model buku

---
+++ app/Buku.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Buku extends Model
+{
+    protected $table = 'tb_buku';
+
+    public function peminjamans(){
+        return $this->hasMany('App\Peminjaman');
+    }
+}
