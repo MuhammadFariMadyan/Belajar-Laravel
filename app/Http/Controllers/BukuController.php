e5ecc8460c4ba552d518d93029cc9f9b530fd163
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-05 23:13:27 +0700

    tambah function simpan buku

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -6,6 +6,7 @@ use App\Buku;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Ramsey\Uuid\Uuid;
 
 class BukuController extends Controller
 {
@@ -13,5 +14,27 @@ class BukuController extends Controller
     {
         $buku = Buku::paginate(10);
         return view('buku.index', ['bukus' => $buku]);
+    }
+
+    public function tambahBuku()
+    {
+        return view('buku.create');
+    }
+
+    public function simpanBuku(Request $request)
+    {
+        $buku = new Buku;
+
+        $buku->id_buku = Uuid::uuid4();
+        $buku->judul_buku = $request->judul_buku;
+        $buku->pengarang = $request->pengarang;
+        $buku->tahun_terbit = $request->tahun_terbit;
+        $buku->penerbit = $request->penerbit;
+        $buku->jumlah_buku = $request->jumlah_buku;
+        $buku->nomor_rak_buku = $request->nomor_rak_buku;
+
+        $buku->save();
+
+        return redirect('Buku');
     }
 }
