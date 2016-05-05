edce0f47937a23b4ff71e57dd2a67ab05e79216b
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-05 21:05:45 +0700

    tampilkan data mahasiswa

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Mahasiswa;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
 class MahasiswaController extends Controller
 {
-    //
+    public function index()
+    {
+        $mahasiswa = Mahasiswa::paginate(10);
+        return view('mahasiswa.index', ['mahasiswas' => $mahasiswa]);
+    }
 }
