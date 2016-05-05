a523fad74cdab811b7fb4d5f4febf723dc2db2e7
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-05 23:13:09 +0700

    tambah fill yang boleh di insert

--- app/Buku.php
+++ app/Buku.php
@@ -8,7 +8,12 @@ class Buku extends Model
 {
     protected $table = 'tb_buku';
 
-    public function peminjamans(){
+    protected $fillable = [
+        'judul_buku', 'pengarang', 'tahun_terbit', 'penerbit', 'jumlah_buku', 'nomor_rak_buku'
+    ];
+
+    public function peminjamans()
+    {
         return $this->hasMany('App\Peminjaman');
     }
 }
