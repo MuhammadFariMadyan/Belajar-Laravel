a9a9eb24d63ce98370202947833044df3b685a73
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-05 18:52:44 +0700

    update database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(BukuTableSeeder::class);
+        $this->call(MahasiswaTableSeeder::class);
+        $this->call(PeminjamanTableSeeder::class);
     }
 }
