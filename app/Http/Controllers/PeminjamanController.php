93b74806f791172d50077aefdfbba49f0e7dea6b
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-06 15:16:26 +0700

    tambah function update peminjaman

--- app/Http/Controllers/PeminjamanController.php
+++ app/Http/Controllers/PeminjamanController.php
@@ -59,10 +59,65 @@ class PeminjamanController extends Controller
         $peminjaman->id_peminjaman = Uuid::uuid4();
         $peminjaman->tanggal_peminjaman = $request->tanggal_peminjaman;
         $peminjaman->tanggal_batas_pengembalian = $request->tanggal_batas_pengembalian;
-        $peminjaman->npm = $request->mahasiswa;
-        $peminjaman->id_buku = $request->buku;
+        $peminjaman->npm = $request->npm;
+        $peminjaman->id_buku = $request->id_buku;
 
         $peminjaman->save();
+
+        return redirect('Peminjaman');
+    }
+
+    public function editPeminjaman($idPeminjaman)
+    {
+        $mahasiswas = Mahasiswa::lists('nama', 'npm');
+        $bukus = Buku::lists('judul_buku', 'id_buku');
+
+        $peminjaman = \DB::table('tb_peminjaman')
+            ->join('tb_buku', 'tb_peminjaman.id_buku', '=', 'tb_buku.id_buku')
+            ->join('tb_mahasiswa', 'tb_peminjaman.npm', '=', 'tb_mahasiswa.npm')
+            ->select(
+                'tb_peminjaman.tanggal_batas_pengembalian',
+                'tb_peminjaman.tanggal_peminjaman',
+                'tb_peminjaman.id_peminjaman',
+                'tb_mahasiswa.npm',
+                'tb_mahasiswa.nama',
+                'tb_mahasiswa.kelas',
+                'tb_buku.judul_buku',
+                'tb_buku.pengarang',
+                'tb_buku.tahun_terbit',
+                'tb_buku.penerbit',
+                'tb_buku.nomor_rak_buku',
+                'tb_buku.id_buku'
+            )
+            ->where('id_peminjaman', $idPeminjaman)->first();
+
+        return view('peminjaman.edit')
+            ->with('peminjaman', $peminjaman)
+            ->with([
+                'mahasiswas' => $mahasiswas,
+                'bukus' => $bukus
+            ]);
+    }
+
+    public function updatePeminjaman(Request $request, $idPeminjaman)
+    {
+
+        $peminjaman = Peminjaman::where('id_peminjaman', $idPeminjaman)->firstOrFail();
+
+        $this->validate($request, [
+            'tanggal_peminjaman' => 'required|date',
+            'tanggal_batas_pengembalian' => 'required|date',
+            'npm' => 'required',
+            'id_buku' => 'required'
+        ]);
+
+        Peminjaman::where('id_peminjaman', $peminjaman->id_peminjaman)
+            ->update([
+                'tanggal_peminjaman' => $request->tanggal_peminjaman,
+                'tanggal_batas_pengembalian' => $request->tanggal_batas_pengembalian,
+                'npm' => $request->npm,
+                'id_buku' => $request->id_buku
+            ]);
 
         return redirect('Peminjaman');
     }
