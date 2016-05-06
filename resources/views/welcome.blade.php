973d93fcab2010131207543070bc24634d78d541
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-06 10:46:30 +0700

    update home dan navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,13 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+@extends('layouts.aplikasi')
+@section('title', 'Home')
+@section('content')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <div class="jumbotron text-center">
+        <h1>Selamat Datang</h1>
+        <p>Belajar Laravel 5</p>
+        <p>
+            <a class="btn btn-lg btn-primary" href="{{ url('') }}" role="button">Learn More</a>
+        </p>
+    </div>
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
-        </div>
-    </body>
-</html>
+@endsection
