034eac48c69a680dd1aff5ff88fc5594b5bd4133
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-06 14:35:29 +0700

    tambah function update untuk mahasiswa

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -43,4 +43,35 @@ class MahasiswaController extends Controller
 
         return redirect('Mahasiswa');
     }
+
+    public function editMahasiswa($npm)
+    {
+        $mahasiswa = Mahasiswa::where('npm', $npm)->firstOrFail();
+        return view('mahasiswa.edit')
+            ->with('mahasiswa', $mahasiswa);
+    }
+
+    public function updateMahasiswa(Request $request, $npm)
+    {
+
+        $mahasiswa = Mahasiswa::where('npm', $npm)->firstOrFail();
+
+        $this->validate($request, [
+            'npm' => 'required|max:8',
+            'nama' => 'required|max:50',
+            'kelas' => 'required|max:6',
+            'jenis_kelamin' => 'required',
+            'alamat' => 'required'
+        ]);
+
+        Mahasiswa::where('npm', $mahasiswa->npm)
+            ->update([
+                'nama' => $request->nama,
+                'kelas' => $request->kelas,
+                'jenis_kelamin' => $request->jenis_kelamin,
+                'alamat' => $request->alamat
+            ]);
+
+        return redirect('Mahasiswa');
+    }
 }
