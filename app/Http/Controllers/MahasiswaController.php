f9c295cc1c849e6a5aaf1ea473d0431b8870cb9e
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-06 10:00:48 +0700

    tambah simpan mahasiswa

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -14,4 +14,24 @@ class MahasiswaController extends Controller
         $mahasiswa = Mahasiswa::paginate(10);
         return view('mahasiswa.index', ['mahasiswas' => $mahasiswa]);
     }
+
+    public function tambahMahasiswa()
+    {
+        return view('mahasiswa.create');
+    }
+
+    public function simpanMahasiswa(Request $request)
+    {
+        $mahasiswa = new Mahasiswa;
+
+        $mahasiswa->npm = $request->npm;
+        $mahasiswa->nama = $request->nama;
+        $mahasiswa->kelas = $request->kelas;
+        $mahasiswa->jenis_kelamin = $request->jenis_kelamin;
+        $mahasiswa->alamat = $request->alamat;
+
+        $mahasiswa->save();
+
+        return redirect('Mahasiswa');
+    }
 }
